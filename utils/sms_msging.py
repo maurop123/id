33192b992e6dd0b82ec38b9236ec867d7ba15532
mauro <mauro@mauro-portatil.(none)>
2014-03-04 09:21:13 -0500

    removed sensitive information from project

--- utils/sms_msging.py
+++ utils/sms_msging.py
@@ -1,4 +1,4 @@
-import smtplib, re
+import smtplib, re, ConfigParser
 from datetime import datetime
 
 class msging():
@@ -7,17 +7,19 @@ class msging():
   provider_handles = {
     'tmobile': 'tmomail.net'
   }
+  config = ConfigParser.ConfigParser()
 
   def __init__(self, number=None, provider='tmobile'):
     if number:
       self.phone_number = number
       self.set_email(self.phone_number, provider)
+    self.config.readfp(open('config.cfg'))
 
   def send_sms(self, msg):
     server = smtplib.SMTP()
     server.connect('smtp.gmail.com', 587)
     server.starttls()
-    server.login('maurop123@gmail.com', 'jnxmamihhrljxkwa')
+    server.login(self.config.get('default','username'), self.config.get('default', 'password'))
     message = u'From: {0}\nTo: {1}\nSubject: {2}\nDate: {3}\n\n{4}'.format('maurop123@gmail.com', self.email, 'foobar', datetime.now(), msg)
     server.sendmail('maurop123@gmail.com',self.email,message)
     server.quit()
